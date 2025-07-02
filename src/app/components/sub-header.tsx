cb00b31ef37e9225c1ddb5e87fa14e328b849c2b
nitin <nitin.gautam@digital.datamatics.com>
2025-07-02 18:08:28 +0530

    navingation and overlapping issue fixes

--- src/app/components/sub-header.tsx
+++ src/app/components/sub-header.tsx
@@ -99,7 +99,7 @@ const SubHeader = () => {
   };
 
   return (
-    <nav className="sub-header ">
+    <nav className="sub-header">
       <div className="container d-flex justify-content-between align-items-center position-relative p-1">
         {/* Navigation Links - Centered */}
         <ul className="d-flex mb-0 position-absolute start-50 translate-middle-x">
